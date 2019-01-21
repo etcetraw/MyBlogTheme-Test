7e95c66dd67146cbd15d6871a1135b9ff55c702b
unknown <etcetraw@gmail.com>
2019-01-21 13:35:18 -0700

    Theme fails with newest nuget packages

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "My Blog Theme",
+    Author = "My Orchard Team",
+    Website = "http://orchardproject.net",
+    Version = "2.0.0",
+    Description = "My theme adapted for blogs."
+)]
